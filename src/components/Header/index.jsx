bba4c4f88c90494c8f52ecdf189b078dceed588a
FranciscoJose28 <fcojmatos28@gmail.com>
2024-04-04 11:55:13 -0300

    continuando rotas

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+        <>
+            <h1>LOGO</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/about">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+ 
+export default Header;
